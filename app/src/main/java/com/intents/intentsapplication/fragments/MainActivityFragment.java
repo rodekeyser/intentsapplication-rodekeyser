193364e2c544b836d9712340ac351b286cc4f1c0
Robin De Keyser <robin.dekeyser.v5909@student.hogent.be>
2017-01-03 18:44:40 +0100

    changed output text

--- app/src/main/java/com/intents/intentsapplication/fragments/MainActivityFragment.java
+++ app/src/main/java/com/intents/intentsapplication/fragments/MainActivityFragment.java
@@ -1,12 +1,15 @@
 package com.intents.intentsapplication.fragments;
 
 import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,12 +22,14 @@ import com.google.zxing.integration.android.IntentResult;
 import com.intents.intentsapplication.R;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 import static android.app.Activity.RESULT_OK;
+import static android.content.ContentValues.TAG;
 
 
 public class MainActivityFragment extends Fragment {
@@ -53,7 +58,27 @@ public class MainActivityFragment extends Fragment {
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "nl-NL");
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         //intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "text to show");
-
+        PackageManager pm = getActivity().getPackageManager();
+        ComponentName cn = intent.resolveActivity(pm);
+        if (cn == null) {
+            // If there is no Activity available to perform the action
+            // Check to see if the Google Play Store is available.
+            Uri marketUri =
+                    Uri.parse("market://search?q=pname:com.intentsapplication.intents");
+            Intent marketIntent = new Intent(Intent.ACTION_VIEW).setData(marketUri);
+            // If the Google Play Store is available, use it to download an application
+            // capable of performing the required action. Otherwise log an
+            // error.
+            if (marketIntent.resolveActivity(pm) != null){
+                startActivity(marketIntent);
+            }
+            else{
+                Log.d(TAG, "Market client not available.");
+            }
+        }
+        else {
+            startActivity(intent);
+        }
         try
         {
             startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
@@ -79,8 +104,18 @@ public class MainActivityFragment extends Fragment {
                     if (resultCode == RESULT_OK && null != data)
                     {
                         ArrayList<String> resultSTT = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                            String zin = resultSTT.get(0);
-                            outputText.setText(zin);
+                        String zin = resultSTT.get(0);
+                        Random rn = new Random();
+                        int i = rn.nextInt(4)+1;
+                        if(i == 1){
+                            outputText.setText("I can't speak dutch very well but I think you said: \"" + zin + "\".");
+                        } else if(i == 2){
+                            outputText.setText("Did you just say: \"" + zin + "\" in dutch?");
+                        } else if(i == 3){
+                            outputText.setText("\" " + zin.toUpperCase() + "!! \" ... Wow, you don't have to yell at me!?");
+                        } else if(i == 4){
+                            outputText.setText("\" " + zin.toLowerCase().replaceAll("r", "l") + " \" ... I didn't know you were chinese.");
+                        }
 
                     }
                     break;
